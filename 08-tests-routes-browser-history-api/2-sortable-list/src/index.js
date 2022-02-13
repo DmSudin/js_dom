1b40a2167817344f1304c846a75ab718ef7ca9ee
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-02-13 22:18:23 +0300

    lesson 8-2

--- 08-tests-routes-browser-history-api/2-sortable-list/src/index.js
+++ 08-tests-routes-browser-history-api/2-sortable-list/src/index.js
@@ -1,3 +1,51 @@
 export default class SortableList {
+    element;
+
+    onPointerDown(event) {
+        const elem = event.target.closest('span[data-grab-handle]');
+        if (elem) {
+            console.error('elem', elem);
+            const li = elem.closest('li.sortable-list__item');
+            console.error('li', li);
+            const placeholder = li.cloneNode();
+            document.body.append(placeholder);
+            //todo continue
+        }
+        
+
+    }
+
+    constructor(elementList) {
+        this.elementList = elementList.items;
+        this.element = this.render();
+        
+    }
+
+    render() {
+        const ul = document.createElement('ul');        
+        ul.classList.add('sortable-list');
+        ul.innerHTML = this.template();
+        this.element = ul;
+        this.initEventListeners();
+        return this.element;
+    }
+
+    template () {
+        debugger
+        let result = '';
+        this.elementList.map(item => {
+            item.classList.add('sortable-list__item')
+        });
+        this.elementList.forEach(element => {
+            result += element.outerHTML;    
+        });
+        return result;
+    }
+
+    initEventListeners() {
+        this.element.addEventListener('pointerdown', event => {
+            this.onPointerDown(event);
+        });
+    }
 
 }
